e409ba29590af801125797c26612e5372b98286d
Kouki Saito <dan.addr.skd@gmail.com>
2014-05-17 16:51:19 +0900

    Add ContentRootViewController for page view’s root controller.

---
+++ Altstars/Altstars/ContentRootViewController.h
@@ -0,0 +1,15 @@
+//
+//  ContentRootViewController.h
+//  Altstars
+//
+//  Created by Kouki Saito on 2014/05/17.
+//  Copyright (c) 2014年 Kouki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentRootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
---
+++ Altstars/Altstars/ContentRootViewController.m
@@ -0,0 +1,49 @@
+//
+//  ContentRootViewController.m
+//  Altstars
+//
+//  Created by Kouki Saito on 2014/05/17.
+//  Copyright (c) 2014年 Kouki. All rights reserved.
+//
+
+#import "ContentRootViewController.h"
+
+@interface ContentRootViewController ()
+
+@end
+
+@implementation ContentRootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
