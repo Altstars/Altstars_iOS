937882a2fea65ee8bad2dee610fbeb9b55faac92
Kouki Saito <dan.addr.skd@gmail.com>
2014-05-17 16:49:35 +0900

    Add content view for page view’s content

---
+++ Altstars/Altstars/ContentDataViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContentDataViewController.h
+//  Altstars
+//
+//  Created by Kouki Saito on 2014/05/17.
+//  Copyright (c) 2014年 Kouki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentDataViewController : UIViewController
+
+@end
